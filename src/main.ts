e1bd39bc4c2f491a97b2404b77e1b39c27454e04
Jack O'Brien <jadyobri@ucsc.edu>
2024-10-21 17:39:37 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import "./style.css";
 const APP_NAME = "Project 2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+window.onload = () => {
+    const appTitle = document.getElementById('app-title');
+    if (appTitle) {
+        appTitle.textContent = "My Awesome App";
+    }
+};
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
